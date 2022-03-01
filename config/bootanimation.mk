e51dce20dafb9f94db4478f2e62b60f00febf2e6
ghou8s <ghou8s@gmail.com>
2022-03-01 13:40:05 +0000

    Update new bootanimation from colt a12

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -16,28 +16,15 @@
 
 TARGET_BOOT_ANIMATION_RES ?= undefined
 
-ifeq ($(TARGET_BOOT_ANIMATION_RES),720x1280)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/720x1280.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),768x1280)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/768x1280.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080x1920)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080x1920.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080x2160)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080x2160.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080x2246)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080x2246.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080x2248)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080x2248.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080x2280)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080x2280.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080x2340)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080x2340.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440x2560)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1440x2560.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440x2880)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1440x2880.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440x2960)
-     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1440x2960.zip:system/media/bootanimation.zip
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1080.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/1440.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),768)
+     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/768.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/720.zip:system/media/bootanimation.zip
+
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
      PRODUCT_COPY_FILES += vendor/colt/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
